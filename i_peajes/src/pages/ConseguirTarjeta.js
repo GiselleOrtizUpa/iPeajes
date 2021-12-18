279bcfa98569d044e5c8b9bc4915a4b68f66a207
jespitiaau <91152504+jespitiaau@users.noreply.github.com>
2021-12-17 22:07:27 -0500

    conseguir tarjeta

--- i_peajes/src/pages/ConseguirTarjeta.js
+++ i_peajes/src/pages/ConseguirTarjeta.js
@@ -25,6 +25,39 @@ const ConseguirTarjeta = () => {
 
                             {/* <!-- Page Heading --> */}
                             <h1 class="h3 mb-4 text-gray-800">Conseguir Tarjeta</h1>
+                            <form>
+                            <div class="row mb-3">
+                                    <label for="inputNombre" class="col-sm-2 col-form-label">Nombre</label>
+                                    <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="inputNombre" name="usuarios_nombre"></input>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="inputApellido" class="col-sm-2 col-form-label">Apellido</label>
+                                    <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="inputApellido" name="usuarios_apellido"></input>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="inputEmail" class="col-sm-2 col-form-label">Correo</label>
+                                    <div class="col-sm-10">
+                                    <input type="email" class="form-control" id="inputEmail" name="usuarios_correo"></input>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="inputUsuario" class="col-sm-2 col-form-label">Numero Tarjeta:</label>
+                                    <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="inputUsuario" name="usuarios_usuario"></input>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="inputUsuario" class="col-sm-2 col-form-label">Saldo inicial</label>
+                                    <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="inputUsuario" name="usuarios_usuario"></input>
+                                    </div>
+                                </div>
+                                <button type="button" class="btn btn-primary btn-lg">Procesar</button>
+                                </form>
 
                         </div>
                         {/* <!-- /.container-fluid --> */}
