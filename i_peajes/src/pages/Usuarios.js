c7b2ebaec4a657590c2ed696ed5b5a03961a5046
maleja2102 <male.lavado.2102@gmail.com>
2021-12-17 20:57:04 -0500

    Interfaz usuarios

--- i_peajes/src/pages/Usuarios.js
+++ i_peajes/src/pages/Usuarios.js
@@ -25,7 +25,90 @@ const Usuarios = () => {
 
                             {/* <!-- Page Heading --> */}
                             <h1 class="h3 mb-4 text-gray-800">Usuarios</h1>
-
+                            <div class="table-responsive">
+                            <table class="table table-bordered  table-striped table-responsive" id="usertable">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Apellido</th>
+                                    <th scope="col">Usuario</th>
+                                    <th scope="col"hidden>Contraseña</th>
+                                    <th scope="col"hidden>Rol</th>
+                                    <th scope="col">Cédula</th>
+                                    <th scope="col"hidden>Correo</th>
+                                    <th scope="col">Cargo</th>
+                                    <th scope="col" hidden>Imagen</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <td>informacion</td>
+                                <td>informacion</td>
+                                <td>informacion</td>
+                                <td>informacion</td>
+                                <td>informacion</td>
+                            </tbody>
+                            </table>
+                            </div>
+                            <form>
+                            <div class="row mb-3">
+                                    <label for="inputNombre" class="col-sm-2 col-form-label">Nombre</label>
+                                    <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="inputNombre" name="usuarios_nombre"></input>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="inputApellido" class="col-sm-2 col-form-label">Apellido</label>
+                                    <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="inputApellido" name="usuarios_apellido"></input>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="inputEmail" class="col-sm-2 col-form-label">Correo</label>
+                                    <div class="col-sm-10">
+                                    <input type="email" class="form-control" id="inputEmail" name="usuarios_correo"></input>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="inputUsuario" class="col-sm-2 col-form-label">Usuario</label>
+                                    <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="inputUsuario" name="usuarios_usuario"></input>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="inputPassword" class="col-sm-2 col-form-label">Contraseña</label>
+                                    <div class="col-sm-4">
+                                    <input type="password" class="form-control" id="inputPassword" name="usuarios_clave"></input>
+                                    </div>
+                                    <label for="inputPassword" class="col-sm-2 col-form-label">Confirmar</label>
+                                    <div class="col-sm-4">
+                                    <input type="password" class="form-control" id="inputConfirmar" name="usuarios_confirmar"></input>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="inputCedula" class="col-sm-2 col-form-label">Cédula</label>
+                                    <div class="col-sm-10">
+                                    <input type="number" class="form-control" id="inputCedula" name="usuarios_cedula"></input>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="inputCargo" class="col-sm-2 col-form-label">Rol</label>
+                                    <div class="col-sm-4">
+                                    <select class="form-control" name="usuarios_rol" id="inputRol" >
+                                        <option value="administrador">Administrador</option>
+                                        <option value="interno">Usuario interno</option>
+                                        <option value="externo">Usuario externo</option>
+                                    </select>
+                                    </div>
+                                </div>
+                            </form>
+                            <div class="row mb-3 d-flex justify-content-center">
+                                    <div class="btn-group btn-group-justified">
+                                        <button type="submit" class="btn btn-success btn-lg">Agregar</button>
+                                        <button type="submit" class="btn btn-warning btn-lg">Editar</button>
+                                        <button type="submit" class="btn btn-danger btn-lg">Eliminar</button>
+                                        <input type="reset" value="Limpiar formulario" class="btn btn-dark"></input>
+                                    </div>
+                            </div>
                         </div>
                         {/* <!-- /.container-fluid --> */}
 
