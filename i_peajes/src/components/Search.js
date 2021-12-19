7e2675437ed4a508feacd4c51fe3f4bef6fb39ca
maleja2102 <male.lavado.2102@gmail.com>
2021-12-18 19:50:16 -0500

    Logotipos

--- i_peajes/src/components/Search.js
+++ i_peajes/src/components/Search.js
@@ -5,10 +5,10 @@ const Search = () => {
         <div class="input-group mb-3">
             <input type="text" class="form-control" placeholder="Buscar..." id="searchbox" name="searchbox"></input>
             <button type="submit" class="input-group-text shadow-none px-4 btn-primary">
-                <i class="fa-solid fa-magnifying-glass"></i>
+                <i class="fa-regular fa-rotate"></i>Buscar
             </button>
             <button type="submit" class="input-group-text shadow-none px-4 btn-dark">
-                <i class="fa-solid fa-arrows-rotate"></i>
+                <i class="fa-solid fa-loader"></i>Refrescar
             </button>
         </div>
     );
